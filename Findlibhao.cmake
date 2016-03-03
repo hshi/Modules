27f83057d34dc8deee235ed1118d1ecbc915644a
boruoshihao <boruoshihao@gmail.com>
2016-03-03 15:29:46 -0500

    update Findlibhao.cmake to include thread_mcdata_manipulate_lib_hao_1

--- Findlibhao.cmake
+++ Findlibhao.cmake
@@ -1,15 +1,16 @@
 #Try to find libhao
 set(libhao_path "$ENV{LIBHAO}")
-find_library(fftwhao_libraries     NAMES  libfftwhao.a      PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
-find_library(kahanerrhao_libraries NAMES  libkahanerrhao.a  PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
-find_library(latticehao_libraries  NAMES  liblatticehao.a   PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
-find_library(mathhao_libraries     NAMES  libmathhao.a      PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
-find_library(matrixhao_libraries   NAMES  libmatrixhao.a    PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
-find_library(mpihao_libraries      NAMES  libmpihao.a       PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
-find_library(randomhao_libraries   NAMES  librandomhao.a    PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
-find_library(timehao_libraries     NAMES  libtimehao.a      PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
-set(libhao_libraries ${fftwhao_libraries} ${kahanerrhao_libraries} ${latticehao_libraries} ${mathhao_libraries} 
-                ${matrixhao_libraries} ${mpihao_libraries} ${randomhao_libraries} ${timehao_libraries})
+find_library(fftwhao_libraries         NAMES  libfftwhao.a           PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
+find_library(kahanerrhao_libraries     NAMES  libkahanerrhao.a       PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
+find_library(latticehao_libraries      NAMES  liblatticehao.a        PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
+find_library(mathhao_libraries         NAMES  libmathhao.a           PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
+find_library(matrixhao_libraries       NAMES  libmatrixhao.a         PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
+find_library(mpihao_libraries          NAMES  libmpihao.a            PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
+find_library(randomhao_libraries       NAMES  librandomhao.a         PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
+find_library(timehao_libraries         NAMES  libtimehao.a           PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
+find_library(threadmcdatahao_libraries NAMES  libthreadmcdatahao.a   PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
+set(libhao_libraries ${threadmcdatahao_libraries} ${fftwhao_libraries} ${kahanerrhao_libraries} ${latticehao_libraries} 
+                     ${mathhao_libraries} ${matrixhao_libraries} ${mpihao_libraries} ${randomhao_libraries} ${timehao_libraries})
 find_path(libhao_include_dirs time_hao.h PATHS "${libhao_path}/include" NO_DEFAULT_PATH)
 
 include(FindPackageHandleStandardArgs)
