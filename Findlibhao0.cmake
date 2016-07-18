5cbae2f706df4f862d457a985a840a3ef006b9e8
Hao Shi <boruoshihao@gmail.com>
2016-07-18 16:47:49 -0400

    Find tensor_hao instead of matrix_hao.

--- Findlibhao0.cmake
+++ Findlibhao0.cmake
@@ -4,12 +4,12 @@ find_library(fftwhao_libraries         NAMES  libfftwhao.a           PATHS "${li
 find_library(kahanerrhao_libraries     NAMES  libkahanerrhao.a       PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
 find_library(latticehao_libraries      NAMES  liblatticehao.a        PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
 find_library(mathhao_libraries         NAMES  libmathhao.a           PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
-find_library(matrixhao_libraries       NAMES  libmatrixhao.a         PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
+find_library(tensorhao_libraries       NAMES  libtensorhao.a         PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
 find_library(mpihao_libraries          NAMES  libmpihao.a            PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
 find_library(randomhao_libraries       NAMES  librandomhao.a         PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
 find_library(timehao_libraries         NAMES  libtimehao.a           PATHS "${libhao_path}/lib" NO_DEFAULT_PATH)
 set(libhao0_libraries ${fftwhao_libraries} ${kahanerrhao_libraries}  ${latticehao_libraries}  ${mathhao_libraries} 
-                     ${matrixhao_libraries} ${mpihao_libraries} ${randomhao_libraries} ${timehao_libraries} )
+                     ${tensorhao_libraries} ${mpihao_libraries} ${randomhao_libraries} ${timehao_libraries} )
 find_path(libhao0_include_dirs time_hao.h PATHS "${libhao_path}/include" NO_DEFAULT_PATH)
 
 include(FindPackageHandleStandardArgs)
